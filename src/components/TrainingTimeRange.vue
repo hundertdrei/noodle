16460c4ca22a021729540d829a642c8c1f05be4e
Frank Richter <frank.richter@gmail.com>
2022-02-13 13:46:43 +0100

    Add component for displaying the training time range

---
+++ src/components/TrainingTimeRange.vue
@@ -0,0 +1,19 @@
+<template>
+  <span>
+    <Alt :x="defaultObj.timeBegin" :y="altObj ? altObj.timeBegin : null" :format="formatTime"/>
+      - 
+    <Alt :x="defaultObj.timeEnd" :y="altObj ? altObj.timeEnd : null" :format="formatTime"/>
+  </span>
+</template>
+
+<script>
+export default {
+  name: "TrainingTimeRange",
+  props: ["defaultObj", "altObj"],
+  computed: {
+    formatTime () {
+      return function (x) { return this.$options.filters.timejs(x, "HH:mm") }
+    }
+  },
+};
+</script>
