746fd111fd5358629de20e516653d9d02df6324b
Frank Richter <frank.richter@gmail.com>
2022-09-24 18:40:27 +0200

    TrainingTimeRange: * Change layout from "~~old~~new - ~~old~~new" to "~~old - old~~new - new" * Strike the whole range through when only beginning or end is overridden

--- src/components/TrainingTimeRange.vue
+++ src/components/TrainingTimeRange.vue
@@ -1,8 +1,11 @@
 <template>
   <span>
-    <Alt :x="defaultObj.timeBegin" :y="altObj ? altObj.timeBegin : null" :format="formatTime"/>
-      - 
-    <Alt :x="defaultObj.timeEnd" :y="altObj ? altObj.timeEnd : null" :format="formatTime"/>
+    <span :class="{strike: showAlternative}">
+        {{ formatTime(defaultObj.timeBegin) }} - {{ formatTime(defaultObj.timeEnd) }}
+    </span>
+    <span v-if="showAlternative">
+        {{ formatTime(altObj.timeBegin ? altObj.timeBegin : defaultObj.timeBegin) }} - {{ formatTime(altObj.timeEnd ? altObj.timeEnd : defaultObj.timeEnd) }}
+    </span>
   </span>
 </template>
 
@@ -11,9 +14,19 @@ export default {
   name: "TrainingTimeRange",
   props: ["defaultObj", "altObj"],
   computed: {
+    showAlternative () {
+        console.log(this.altObj, this.altObj.timeBegin, this.altObj.timeEnd)
+        return this.altObj && (this.altObj.timeBegin || this.altObj.timeEnd)
+    },
     formatTime () {
       return function (x) { return this.$options.filters.timejs(x, "HH:mm") }
     }
   },
 };
 </script>
+
+<style scoped>
+.strike {
+    text-decoration: line-through;
+}
+</style>
